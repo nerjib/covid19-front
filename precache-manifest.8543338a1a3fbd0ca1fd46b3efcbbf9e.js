0bf96d6536de1cb67cb79b62dbd14b11d2397b00
nerjib <kabirnajib0@gmail.com>
2020-04-11 01:13:24 +0000

    Updates

---
+++ precache-manifest.8543338a1a3fbd0ca1fd46b3efcbbf9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3877f1022e290fe037a472a0538d84",
+    "url": "/covid19-front/index.html"
+  },
+  {
+    "revision": "7f035e29ff6d1d3baf8a",
+    "url": "/covid19-front/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "a50f40a71a84697d2810",
+    "url": "/covid19-front/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "7f035e29ff6d1d3baf8a",
+    "url": "/covid19-front/static/js/2.2eb2d2e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19-front/static/js/2.2eb2d2e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50f40a71a84697d2810",
+    "url": "/covid19-front/static/js/main.0c44099e.chunk.js"
+  },
+  {
+    "revision": "f1f1637ef15fe80d81f3",
+    "url": "/covid19-front/static/js/runtime-main.f8000e9d.js"
+  }
+]);
